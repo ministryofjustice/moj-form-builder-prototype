8c7878fbddf6d2ab2dde1bef0d0729cb47fc94d7
Adam Silver <adambsilver@gmail.com>
2019-09-17 09:08:05 +0100

    Updated prototype to automatically version urls based on first url param

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,12 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.use('/:version/', function (req, res, next) {
+  const version = `/${req.params.version}`;
+  res.locals.getUrl = function(url) {
+    return version+url;
+  };
+  next();
+});
+
 module.exports = router
